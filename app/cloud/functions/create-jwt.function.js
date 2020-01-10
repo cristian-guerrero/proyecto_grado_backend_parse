de1fd94b0933697f6227615ca47673f9ad9fad5d
cristian-guerrero <guerrero.chavez.cristian@gmail.com>
2020-01-10 10:33:53 -0500

    added new cloud function to create sniffer token

--- app/cloud/functions/create-jwt.function.js
+++ app/cloud/functions/create-jwt.function.js
@@ -8,33 +8,100 @@ const fs = require('fs')
 const jwt = require('../util/jwt.service')
 
 /**
- * 
- * 
+ * Cloud Function para crear un registro en la clase Token
+ *
+ *
+ *
+ *
+ *
+ *
  */
-Parse.Cloud.define('_create_jwt', async (request) => {
+Parse.Cloud.define('_create_sniffer_token', async (request) => {
 
-  throw new Error('No implementado')
   const { user, params } = request
 
   if (!user) { throw new Error('Debe estar logueado para realizar esta acción') }
 
-  const { payLoad } = params
-
-  payLoad.expiresIn = getDiffInSeconds( payLoad.expiresIn )
+  const { expiry, sniffer } = params
 
 
-  return payLoad
+
+  try {
+    //
+    const expireIn = getDiffInSeconds(expiry)
+
+    const newToken = await newTokenDocument(sniffer, user, expiry)
+
+
+    const hash = await  generateJwt(sniffer, newToken.id,  expireIn)
+
+    const tokenUpdated = await  updateNewTokenDocument(newToken, hash)
+
+
+
+    console.log(tokenUpdated)
+    return tokenUpdated
+
+  } catch (e) {
+    console.log(e)
+    throw  e
+  }
+
 })
 
 /**
- *
+ * Crear un nuevo registro en la clase Token sin el JWT
+ * @param snifferId
+ * @param createdBy
+ * @param expiry
+ * @returns {Promise<Object>}
+ */
+async function newTokenDocument(snifferId, createdBy, expiry) {
+
+  const sniffer = Parse.Object.createWithoutData(snifferId)
+  sniffer.className = 'Sniffer'
+
+
+  const ob = new Parse.Object('Token')
+  ob.set({
+    sniffer,
+    createdBy,
+    expiry
+  })
+
+
+  return await ob.save(null, {useMasterKey: true })
+}
+
+/**
+ * Actualiza un registro en la clase Token agregandole el JWT
+ * @param tokenObject
+ * @param hash
+ * @returns {Promise<*>}
+ */
+async function  updateNewTokenDocument(tokenObject, hash) {
+
+  tokenObject.set({hash})
+
+  return await tokenObject.save(null, {useMasterKey: true })
+}
+
+
+
+/**
+ * Devuelve la diferencia en segundos entre la fecha que llega como parametro
+ * y la fecha de hoy
  *
  * @param {*} newDate
  * @returns
  */
 function getDiffInSeconds(newDate) {
 
-  const today = moment()
+  if (!newDate) {
+    throw new Error('La fecha de expiración es requerida')
+  }
+  const today = moment().startOf('d')
+  // newDate = moment(newDate).endOf('d')
 
   if (today.isAfter(newDate)) {
     throw new Error('La fecha debe ser mayor a hoy')
@@ -44,31 +111,32 @@ function getDiffInSeconds(newDate) {
   return range.diff('seconds')
 }
 
-// generateJwt().then()
 
 
-function testRange() {
+/**
+ *
+ * @param sniffer
+ * @param id
+ * @param expireIn
+ * @returns {Promise<unknown>}
+ */
+async function generateJwt(sniffer,id,  expireIn) {
 
-  const da = moment('2020-01-30', 'YYYY-MM-DD')
-  console.log(Math.floor(da.toDate() / 1000))
+  const {key, pub} = await readRSAKeys()
 
+  return  await jwt.sign({sniffer, id }, key, expireIn)
 
-  const range = moment.range(moment(), da);
-
-
-  console.log(range.diff('seconds'))
-
-  return range.diff('seconds')
 }
 
-async function generateJwt() {
+/**
+ * todo las claves deben venir de la base de datos
+ *  modificar esta funcionalidad despues
+ * @returns {Promise<{pub: string, key: string}>}
+ */
+async function readRSAKeys() {
 
-  const key = fs.readFileSync(`${appRootPath.path}/jwtRS256.key`, 'utf-8')
-  const pub = fs.readFileSync(`${appRootPath.path}/jwtRS256.key.pub`, 'utf-8')
-
-  const token = await jwt.sign({ sniffer: 'fkrgu66fk4', ip: '192.168.1.200' }, key, testRange())
-  console.log(token)
-
-  console.log(await jwt.verify(token, pub))
-  console.log(await jwt.decode(token))
+  return {
+    key: fs.readFileSync(`${appRootPath.path}/jwtRS256.key`, 'utf-8'),
+    pub: fs.readFileSync(`${appRootPath.path}/jwtRS256.key.pub`, 'utf-8')
+  }
 }
--- app/cloud/triggers/before-save-token.trigger.js
+++ app/cloud/triggers/before-save-token.trigger.js
@@ -7,69 +7,18 @@ const fs = require('fs')
 const jwt = require('../util/jwt.service')
 
 /**
- * Antes de guardar un token se recive la fecha de expiración,
- * el sniffer al cual va a estar asociado
- * se valida la fecha de expiración y que el sniffer este activo
  *
- *
+ * En la clase  Token solo se puede persistir a través de la cloud function `_create_sniffer_token`
  */
 Parse.Cloud.beforeSave('Token', async request => {
 
   const {original, ip, object, user, master, log} = request
 
 
-  const seconds = object.get('expiry')
+    if(!master) {
+      throw new Error('No tiene permiso para realizar esta acción')
+    }
 
 
 })
 
-
-/**
- *
- *
- * @param {*} newDate
- * @returns
- */
-function getDiffInSeconds(newDate) {
-
-  if (!newDate) {
-    throw new Error('La fecha de expiración es requerida')
-  }
-  const today = moment()
-
-  if (today.isAfter(newDate)) {
-    throw new Error('La fecha debe ser mayor a hoy')
-  }
-  const range = moment.range(today, newDate)
-
-  return range.diff('seconds')
-}
-
-generateJwt().then()
-
-
-function testRange() {
-
-  const da = moment('2020-01-30', 'YYYY-MM-DD')
-  // console.log(Math.floor(da.toDate() / 1000))
-
-
-  const range = moment.range(moment(), da)
-
-
-  // console.log(range.diff('seconds'))
-
-  return range.diff('seconds')
-}
-
-async function generateJwt() {
-
-  const key = fs.readFileSync(`${appRootPath.path}/jwtRS256.key`, 'utf-8')
-  const pub = fs.readFileSync(`${appRootPath.path}/jwtRS256.key.pub`, 'utf-8')
-
-  const token = await jwt.sign({sniffer: 'fkrgu66fk4', ip: '192.168.1.200'}, key, testRange())
-  // console.log(token)
-
-  // console.log(await jwt.verify(token, pub))
-  // console.log(await jwt.decode(token))
-}
